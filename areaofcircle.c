028df4661fc3fc1a8ae587085d0673895f486ba9
Benjamin Zhao <sp1a@rpiXX>
2020-01-07 12:25:22 -0800

    activity 2

--- areaofcircle.c
+++ areaofcircle.c
@@ -3,13 +3,20 @@
 #define PI 3.1415926535
 
 float findarea(float radius)
+{
+	float area;
+	area = PI * radius * radius;
+
+	return area;
+}
+
 int main()
 {
 	float rad, i;
 	for (i = 3.5; i < 12.5; i+=0.5)
 	{
 		rad = findarea(i);
-		printf("Area of circle with radius %f is %f",i , rad);
+		printf("Area of circle with radius %f is %f\n",i , rad);
 	}
 
 	return 0;
@@ -17,12 +24,3 @@ int main()
 
 }
 
-
-float findarea(float radius)
-{
-	float area;
-	area = PI * radius;
-
-
-	return area;
-}
